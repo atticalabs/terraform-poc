59f9d2e35116238b6b20124549bdfe0054a74a88
Ansel <ansel86castro@gmail.com>
2020-01-14 17:31:37 -0300

    Skeleton Arquitecture

---
+++ Terraform/Service1.Services/Data/Service1Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service1.Services
+{
+    public class Service1Context:DbContext
+    {
+        public Service1Context(DbContextOptions<Service1Context> options)
+            : base(options)
+        {
+
+        }
+
+      
+    }
+}
---
+++ Terraform/Service2.Services/AccountService.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Service2.Services
+{
+    public class AccountService
+    {
+    }
+}
